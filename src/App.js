3e9ba43cc664a50841633779be8355f79c40763d
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-29 13:20:35 +0300

    Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  useEffect(
+    // 1st arg: side effect (callback function)
+    () => console.log("useEffect called"),
+    // 2nd arg: dependencies array
+    [count]
+  );
+
+  //abdther way to write in array of dependencies as a secind arguemnt
   useEffect(() => {
     console.log("useEffect called");
-  });
+  }, []);
+
+  //updating itle of page using useeffect
+  useEffect(() => {
+    document.title = "dogs";
+  }, [text]);
+
+  //settimeout with useeffect
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import DogPics from "./DogPics";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <App />
+    <DogPics />
+  </>,
+
+  document.getElementById("root")
+);
